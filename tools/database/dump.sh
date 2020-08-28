5e5dca0215bb288e398076255227e985017352b0
abrikos <abrikoz@gmail.com>
2020-08-28 10:16:51 +0900

    dump

--- tools/database/dump.sh
+++ tools/database/dump.sh
@@ -6,6 +6,6 @@ zip academy dump/academy/*
 #cp academy.zip build/.
 unzip -l academy.zip
 mv academy.zip "akademy-${now}.zip"
-scp "akademy-${now}.zip" 192.168.1.148:/home/abrikos/backup/
+scp "akademy-${now}.zip" 192.168.1.148:/home/abrikos/Documents/backup/
 #mv academy.zip /var/www/devportal.yakutia.science/web/
 
