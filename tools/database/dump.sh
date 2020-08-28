2bbf26b9acdc4363d84560c529f667dfa21fd41b
abrikos <abrikoz@gmail.com>
2020-08-28 10:13:12 +0900

    dump

--- tools/database/dump.sh
+++ tools/database/dump.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+now=$(date +"%m_%d_%Y")
 mongodump
 zip academy dump/academy/*
 #cp academy.zip build/.
 unzip -l academy.zip
+scp ~/akademy/academy.zip "192.168.1.148:/home/abrikos/backup/akademy-${now}.zip"
 #mv academy.zip /var/www/devportal.yakutia.science/web/
 
