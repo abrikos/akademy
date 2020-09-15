eee8632046bb41027310335f60eaf27fff00e93c
abrikos <abrikoz@gmail.com>
2020-09-15 11:54:44 +0900

    fix backup ip

--- tools/database/dump.sh
+++ tools/database/dump.sh
@@ -6,7 +6,7 @@ zip academy dump/academy/*
 #cp academy.zip build/.
 unzip -l academy.zip
 mv academy.zip "academy-${now}.zip"
-scp "academy-${now}.zip" 192.168.1.148:/home/abrikos/Documents/backup/academy
-rsync -a --ignore-existing  ~/akademy/uploads/ 192.168.1.148:/home/abrikos/Documents/backup/academy/uploads
+scp "academy-${now}.zip" 192.168.1.193:/home/abrikos/Documents/backup/academy
+rsync -a --ignore-existing  ~/akademy/uploads/ 192.168.1.193:/home/abrikos/Documents/backup/academy/uploads
 #mv academy.zip /var/www/devportal.yakutia.science/web/
 
