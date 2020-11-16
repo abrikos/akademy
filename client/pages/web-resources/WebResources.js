abd5baa6dca0d5ec9a483f0e3253b26e6e48f01c
abrikos <5422791+abrikos@users.noreply.github.com>
2020-11-16 12:34:48 +0900

    web resources added

--- client/pages/web-resources/WebResources.js
+++ client/pages/web-resources/WebResources.js
@@ -4,10 +4,12 @@ export default function (props) {
     return <div>
         <h1>Веб-ресурсы Академии Наук РС(Я)</h1>
         <ul>
+            <li><a href="https://math.asrsya.ru" target="_blank" rel="noopener noreferrer">Научно−исследовательский семинар "Неклассические задачи математической физики"</a></li>
+            <li><a href="https://schools.asrsya.ru" target="_blank" rel="noopener noreferrer">Сеть школ первого Президента Республики Саха (Якутия) М.Е. Николаева</a></li>
             <li><a href="http://mammothportal.org" target="_blank" rel="noopener noreferrer">Мамонтовая фауна</a></li>
             <li><a href="http://portal.mammothportal.org" target="_blank" rel="noopener noreferrer">Портал мамонтовой фауны</a></li>
             <li><a href="https://nec.yakutia.science" target="_blank" rel="noopener noreferrer">Научно-образовательный центр ГБУ "Академия наук Республики Саха (Якутия)"</a></li>
-            <li><a href="https://nras.yakutia.science" target="_blank" rel="noopener noreferrer">Природные ресурсы Арктики и Субарктики</a></li>
+            <li><a href="https://nras.asrsya.ru" target="_blank" rel="noopener noreferrer">Природные ресурсы Арктики и Субарктики</a></li>
         </ul>
     </div>
 }
