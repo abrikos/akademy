6d1914882d1cdc01a5f87f91050a4bcad8f3c358
abrikos <5422791+abrikos@users.noreply.github.com>
2021-09-30 17:43:16 +0900

    Documents by year

--- client/pages/documents/Documents.js
+++ client/pages/documents/Documents.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import AdminLink from "client/components/AdminLink";
+import moment from "moment";
 
 export default function (props) {
     const [list, setList] = useState([])
@@ -14,10 +15,17 @@ export default function (props) {
         props.api('/document/list', filter)
             .then(r => setList(r.list))
     }, [props.type])
+
+    const years = [...new Set(list.map(l=>moment(l.date).format('YYYY')))]
+
     return <div>
-        <h1>Документы {props.type === 'presidium' && 'Президиума'}</h1>
-        <ul>
-            {list.map(l => <li key={l.id}>{l.date} <a href={l.link} target="_blank" rel="noopener noreferrer">{l.header}</a> <AdminLink model={l} {...props}/> </li>)}
-        </ul>
+        <h1>{props.type === 'presidium' ? 'Протоколы Президиума':'Документы'}</h1>
+        {years.map(y=><div key={y}>
+            <strong>{y}</strong><hr/>
+            <ul>
+            {list.filter(l=>moment(l.date).format('YYYY')===y).map(l => <li key={l.id}>{moment(l.date).format('DD.MM.YY')} <a href={l.link} target="_blank" rel="noopener noreferrer">{l.header}</a> <AdminLink model={l} {...props}/> </li>)}
+            </ul>
+        </div>)}
+
     </div>
 }
