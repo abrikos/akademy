7d7dac9a8a24dfa5f440ec2c7b1fa979b2380bc9
Artem Filippov <a.filippov@qtech.ru>
2025-04-09 11:49:22 +0900

    fix update

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd ~/akademy
 git pull
-tools/site-map.sh
-npm run build
-sh tools/move-build.sh
-pm2 restart all
-tools/database/dump.sh
+#tools/site-map.sh
+#npm run build
+#sh tools/move-build.sh
+#pm2 restart all
+#tools/database/dump.sh
